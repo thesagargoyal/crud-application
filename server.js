419b617c129788fcc01d21cb34efec99406e0140
Sagar Goyal <sagargoyal2015@gmail.com>
2021-05-31 07:46:51 -0700

    Added Database

--- server.js
+++ server.js
@@ -1,30 +1,35 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require("body-parser");
+const path = require('path');
+
+const connectDB = require('./server/database/connection');
+
 const app = express();
-const dotenv=require('dotenv');
-const morgan=require("morgan");
-const bodyparser=require("body-parser");
-const path=require("path");
-const bodyParser = require('body-parser');
 
-dotenv.config({path:"config.env"});
-
+dotenv.config( { path : 'config.env'} )
 const PORT = process.env.PORT || 8080
 
+// log requests
 app.use(morgan('tiny'));
 
-app.use(bodyParser.urlencoded({extended:true}));
+// mongodb connection
+connectDB();
 
-app.set('view engine', 'ejs');
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended : true}))
 
-// app.set('views',path.resolve(__dirname,"views"));
+// set view engine
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))
 
-app.use('/css',express.static(path.resolve(__dirname,"assets/css")));
-app.use('/img',express.static(path.resolve(__dirname,"assets/img")));
-app.use('/js',express.static(path.resolve(__dirname,"assets/js")));
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
 
-//load routers
-app.use("/",require("./server/routes/router"));
+// load routers
+app.use('/', require('./server/routes/router'))
 
-app.listen(PORT, ()=>{
-    console.log(`Application started successfully on port`);
-});
+app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
--- server/services/render.js
+++ server/services/render.js
@@ -1,11 +1,29 @@
-exports.homeRoutes = (req,res)=>{
-    res.render("index");
-};
+const axios = require('axios');
 
-exports.add_user = (req,res)=>{
-    res.render("add_user");
+
+exports.homeRoutes = (req, res) => {
+    // Make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+        .then(function(response){
+            res.render('index', { users : response.data });
+        })
+        .catch(err =>{
+            res.send(err);
+        })
+
+    
 }
 
-exports.update_user = (req,res)=>{
-    res.render("update_user");
+exports.add_user = (req, res) =>{
+    res.render('add_user');
+}
+
+exports.update_user = (req, res) =>{
+    axios.get('http://localhost:3000/api/users', { params : { id : req.query.id }})
+        .then(function(userdata){
+            res.render("update_user", { user : userdata.data})
+        })
+        .catch(err =>{
+            res.send(err);
+        })
 }
